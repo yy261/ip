c898ddc3d8c1be843b3ba4f3c685697428c174aa
Zou Yuyang <e1525145@u.nus.edu>
2026-08-23 13:02:45 +0800

    name change and other stuff

---
+++ src/main/java/Haro.java
@@ -0,0 +1,19 @@
+public class Haro {
+    public static void main(String[] args) {
+        String banner = "  _   _                 \n"
+                + " | | | | __ _ _ __ ___  \n"
+                + " | |_| |/ _` | '__/ _ \\ \n"
+                + " |  _  | (_| | | | (_) |\n"
+                + " |_| |_|\\__,_|_|  \\___/ \n";
+        String horizontalLine = "____________________________________________________________";
+
+        System.out.println(horizontalLine);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Haro.");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
