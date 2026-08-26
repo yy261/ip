e2512afe06edaa1cc2ba1c69bf25d657fb9472e8
Zou Yuyang <e1525145@u.nus.edu>
2026-08-26 21:10:41 +0800

    Add ability to mark tasks as done
    Tracks completion with a parallel boolean[] isDone array alongside the
    existing String[] tasks array, per the requirement to not add any new
    classes. list now prints each task's [X]/[ ] status, and a new
    "mark N" command flips a task's status and confirms with the updated
    line.

--- src/main/java/Haro.java
+++ src/main/java/Haro.java
@@ -16,6 +16,7 @@ public class Haro {
         System.out.println(horizontalLine);
 
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
         int taskCount = 0;
 
         Scanner scanner = new Scanner(System.in);
@@ -27,9 +28,16 @@ public class Haro {
                 System.out.println(horizontalLine);
                 break;
             } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    String statusIcon = isDone[i] ? "X" : " ";
+                    System.out.println((i + 1) + ".[" + statusIcon + "] " + tasks[i]);
                 }
+            } else if (input.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(input.substring("mark ".length())) - 1;
+                isDone[taskIndex] = true;
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [X] " + tasks[taskIndex]);
             } else {
                 tasks[taskCount] = input;
                 taskCount++;
